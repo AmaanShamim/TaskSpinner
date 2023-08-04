c26f99f16f22c90249c46f3321d865879b4ddf7d
Amaan <amaans1912@gmail.com>
2023-08-04 18:02:19 +0530

    made two components and added some functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Break from "./components/Break";
+import Session from "./components/Session";
 
 function App() {
+  const [displayTime, setDisplayTime] = useState(25 * 60);
+  const [breakTime, setBreakTime] = useState(5 * 60);
+  const [sessionTime, setSessionTime] = useState(25 * 60);
+
+  const formatTime = (time) => {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    return (
+      (minutes < 10 ? "0" + minutes : minutes) +
+      ":" +
+      (seconds < 10 ? "0" + seconds : seconds)
+    );
+  };
+
+  const changeTime = (amount, type) => {
+    if (type === "break") {
+      if(breakTime <= 60 && amount < 0){
+        return;
+      }
+      setBreakTime((prev) => prev + amount);
+    }else{
+      if(sessionTime <= 60 && amount < 0){
+        return;
+      }
+      setSessionTime((prev) => prev + amount);
+    }
+  };
+
   return (
     <>
+      <Break
+        title={"Break Length"}
+        changeTime={changeTime}
+        type={"break"}
+        time={breakTime}
+        formatTime={formatTime}
+      />
+      <Session
+        title={"Session Length"}
+        changeTime={changeTime}
+        type={"session"}
+        time={sessionTime}
+        formatTime={formatTime}
+      />
+      <h1>{formatTime(displayTime)}</h1>
     </>
   );
 }
---
+++ src/components/Session.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Session({ title, changeTime, type, time, formatTime }) {
+  return (
+    <>
+      <h3>{title}</h3>
+      <div className="handler">
+        <button onClick={() => changeTime(-60, type)}>-</button>
+        <h4>{formatTime(time)}</h4>
+        <button onClick={() => changeTime(60, type)}>+</button>
+      </div>
+    </>
+  );
+}
